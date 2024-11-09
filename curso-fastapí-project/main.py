e597e443fef4caec36323295fb7a1a16395e5690
Cesar Mora <ingtkdev@ingeniotek.com.co>
2024-11-08 20:56:15 -0500

    Challenge class 4, get with time format 12/24

--- curso-fastapí-project/main.py
+++ curso-fastapí-project/main.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 from fastapi import FastAPI
+import time
 
 app = FastAPI()
 
@@ -7,6 +8,26 @@ app = FastAPI()
 async def root():
     return {"Message": "Hello, World"}
 
-@app.get("/time")
-async def time_now():
-    return {"Time": datetime.now()}
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+@app.get("/time/{iso_code}/{format}")
+async def time_now(iso_code: str, format:str):
+    iso =iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = str(ZoneInfo(timezone_str))
+    time_now = time.localtime()
+    # http://localhost:8000/time/co/24
+    if format == "24":
+        formatted_time = time.strftime("%H:%M:%S", time_now)
+        return {"Time": formatted_time, "Zone": tz}
+    # http://localhost:8000/time/co/12
+    if format == "12":
+        formatted_time_am_pm = time.strftime("%I:%M:%S %p", time_now)
+        return {"Time": formatted_time_am_pm, "Zone": tz}
+    
